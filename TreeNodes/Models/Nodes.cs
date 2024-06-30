8ac580e8fcf2a9a5af85f5ad84e9b6ec07df18e6
HP <HP@DESKTOP-25PI7HG>
2024-06-30 12:27:20 +0530

    Commit2: Intermediate commentings.

--- TreeNodes/Models/Nodes.cs
+++ TreeNodes/Models/Nodes.cs
@@ -5,15 +5,45 @@ using System.Web;
 
 namespace TreeNodes.Models
 {
+    /// <summary>
+    /// Main model
+    /// </summary>
     public class Nodes
     {
+        /// <summary>
+        /// Node ID
+        /// </summary>
         public int NodeId { get; set; }
+
+        /// <summary>
+        /// Node name
+        /// </summary>
         public string NodeName { get; set; }
+
+        /// <summary>
+        /// Parent node ID
+        /// </summary>
         public int? ParentNodeId { get; set; }
+
+        /// <summary>
+        /// Is node active
+        /// </summary>
         public bool IsActive { get; set; }
+
+        /// <summary>
+        /// Node issue date
+        /// </summary>
         public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// Child node list
+        /// </summary>
         public List<Nodes> Children { get; set; } = new List<Nodes>();
 
+
+        /// <summary>
+        /// Check is node new
+        /// </summary>
         public bool isNewUser { get; set; }
     }
 }
